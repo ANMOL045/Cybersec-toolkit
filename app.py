94bf5d19d14e582d725c27431477ba6574be1949
anmol <friday@anmol.anmol>
2026-04-01 19:55:05 +0530

    fixed port

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import socket
 import re
 import math
+import os
 from concurrent.futures import ThreadPoolExecutor
 
 app = Flask(__name__)
@@ -127,6 +128,7 @@ def password():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
 
 
